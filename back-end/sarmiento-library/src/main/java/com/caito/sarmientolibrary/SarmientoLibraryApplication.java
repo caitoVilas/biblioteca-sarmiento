9e396d08b87192b7e6538e19d3e5a5838d7b0587
caito <caitocd@gmail.com>
2022-01-13 09:41:51 -0300

    first commit

---
+++ back-end/sarmiento-library/src/main/java/com/caito/sarmientolibrary/SarmientoLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.caito.sarmientolibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarmientoLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarmientoLibraryApplication.class, args);
+	}
+
+}
---
+++ back-end/sarmiento-library/src/test/java/com/caito/sarmientolibrary/SarmientoLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caito.sarmientolibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SarmientoLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
